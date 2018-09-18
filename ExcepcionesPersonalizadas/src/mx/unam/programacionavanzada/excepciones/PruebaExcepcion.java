53095a13b6855ee5ca8d5e2e5040744f73ee79b6
raulgonzalezcz <raul.gonzalezcz@udlap.mx>
2018-09-18 17:52:29 -0500

    HW completed

--- ExcepcionesPersonalizadas/src/mx/unam/programacionavanzada/excepciones/PruebaExcepcion.java
+++ ExcepcionesPersonalizadas/src/mx/unam/programacionavanzada/excepciones/PruebaExcepcion.java
@@ -29,11 +29,11 @@ public class PruebaExcepcion {
 	 * y propagarse fuera del método o del límite del constructor.<br> 
 	 * 
 	 * <br>Las excepciones no verificadas deben reservarse para los errores del sistema que 
-	 * representan alguna condición de falla no controlada (como NullPointer o OutOfMemory), 
+	 * representan alguna condición de falla no controlada (como NullPointer, OutOfMemory, entre otras), 
 	 * provocando que la aplicación deba finalizar con esa excepción. Se espera que 
 	 * ocurran excepciones de este tipo en cualquier 
-	 * parte del programa, por lo que  indican errores no predecibles en la lógica del programa
-	 * que normalmente no deberían ocurrir. Entonces se utilizan para comprobar la consistencia 
+	 * parte del programa, las cuales indican errores no predecibles en la lógica del código
+	 * que normalmente no deberían ocurrir. Por ello se utilizan para comprobar la consistencia 
 	 * interna del programa y solo deberían recuperarse si realmente sabemos cuáles 
 	 * son las implicaciones de esa recuperación. 
 	 * 
@@ -41,7 +41,9 @@ public class PruebaExcepcion {
 	 * estático en el que se esté creando instancias y se desee lanzar una 
 	 * excepción si falla la creación de instancias. Como los bloques estáticos 
 	 * no tienen la palabra clave throw / throws, RuntimeExceptions se vuelve adecuado
-	 * en este caso. Finalmente se resume que RuntimeException, al ser una unchecked exception, establece que:<br>
+	 * en este caso.<br> 
+	 * 
+	 * <br>Finalmente se resume que RuntimeException, al ser una unchecked exception, establece que:<br>
 	 * 
 	 * <br>
 	 * <ul>
@@ -59,8 +61,12 @@ public class PruebaExcepcion {
 	 * throw new RuntimeException(msg) está creando una instancia de RuntimeException
 	 * con el mensaje especificado antes de ser lanzada.<br>
 	 * 
-	 * <br>Ref: https://docs.oracle.com/javase/8/docs/api/index.html?java/lang/RuntimeException.html
-	 * http://www.jtech.ua.es/j2ee/publico/lja-2012-13/sesion03-apuntes.html
+	 * <br>
+	 * <ol> Referencias: 
+	 * <li><a>https://docs.oracle.com/javase/8/docs/api/index.html?java/lang/RuntimeException.html </a></li>
+	 * <li><a>http://www.jtech.ua.es/j2ee/publico/lja-2012-13/sesion03-apuntes.html</a></li>
+	 * </ol>
+	 * <br>
 	 */
 	public void lanzarExcepcionEnTiempoEjecucion(){
 		throw new RuntimeException("Error en tiempo de ejecución");
@@ -69,16 +75,38 @@ public class PruebaExcepcion {
 	/**
 	 * &iquest;Por qu&eacute; no se declara una excepci&oacute;n?<br>
 	 * 
-	 * <br>A pesar de que no existe error en tiempo de compilación, se marcará
-	 * error en tiempo de ejecución dada la división entre 0. En este caso, creo
-	 * que el autor del código se percató que la división entre 0 se refiere a un tipo
-	 * de excepción verificada (ArithmeticException), la cual debe ser atrapada en
-	 * un bloque try-catch durante la ejecución del método dentro del programa<br>
+	 * <br>En este caso, para Java la definición del método y su contenido no tiene en primera instancia 
+	 * errores en la sintaxis (conocidos también como errores en tiempo de compilación,
+	 * son causados cuando el compilador no puede reconocer una sentencia; 
+	 * por lo que se devuelve un mensaje de error, usualmente con una línea de 
+	 * código de referencia). Sin embargo, por la experiencia sabemos que el programa lanzará un error 
+	 * en la lógica (son conocidos como bugs y hacen referencia a errores que normalmente nos toman tiempo 
+	 * hasta encontrarlos. Otros ejemplos son cuando una clase no está en el directorio correcto, 
+	 * olvidar que los índices en Java empiezan en 0, entre otros).<br>
 	 * 
-	 * <br> Por lo cual, considero que el autor decidió establecer la lógica que permite
-	 * causar una excepción en tiempo de ejecución dentro del método, moviendo la carga
-	 * de trabajo para declarar la excepción en la parte del 
-	 * código donde se llamará este método dentro de un bloque try-catch
+	 * <br>Por ello, a pesar de que no existe error en tiempo de compilación, se marcará
+	 * error en tiempo de ejecución dada la división entre 0. Entonces creo
+	 * que el autor del código se percató que la división entre 0 se refiere a un tipo
+	 * de excepción verificada (ArithmeticException), la cual puede ser atrapada en
+	 * un bloque try-catch cuando se invoque la ejecución del método dentro del programa con el fin
+	 * de manejar adecuadamene esta sentencia susceptible de lanzar una excepción, evitando
+	 * que el programa termine su ejecución repentinamente por este bug.<br>
+	 * 
+	 * <br>Por lo cual, considero que el autor decidió únicamente establecer, a manera de ejemplo,
+	 * la lógica que 
+	 * causa un error lógico en tiempo de ejecución dentro del método, moviendo la carga
+	 * de trabajo para atrapar la excepción en la parte del código donde se llamará 
+	 * este método (en la función principal main) dentro de un bloque try-catch y permitir que 
+	 * la ejecución del código continúe, reconociendo oportunamente la existencia de esta excepción que puede causar
+	 * el colpaso inmediato de nuestro programa.<br>
+	 * 
+	 * <br>
+	 * <ol> Referencias: 
+	 * <li><a>http://www.ciberaula.com/articulo/errores_comunes_programacion_java/ </a></li>
+	 * <li><a>http://www.jtech.ua.es/j2ee/publico/lja-2012-13/sesion03-apuntes.html</a></li>
+	 * </ol>
+	 * <br>
+	 
 	 */
 	public void provocarExcepcionEnTiempoEjecucion(){
 		System.out.println(33/0);
@@ -102,24 +130,36 @@ public class PruebaExcepcion {
 			e.printStackTrace();
 			System.err.println("Código de error: " + e.getCodigo());
 			System.err.println("Mensaje de error: " + e.getMessage());
-		}
-		
-		// Continua programa porque prueba.provocarMiExcepcion() condujo al bloque catch
-		try {
-		System.out.println("Llamando la funci&oacute;n lanzarExcepcionEnTiempoEjecucion() fuera del bloque try-catch");
-		prueba.lanzarExcepcionEnTiempoEjecucion();
-		}catch(RuntimeException re) {
+		} catch(ArithmeticException ae) {
+			ae.printStackTrace();
+			System.err.println("Mensaje de error: " + ae.getMessage());
+		} catch(RuntimeException re) {
 			re.printStackTrace();
 			System.err.println("Mensaje de error: " + re.getMessage());
 		}
 		
+		// Continua programa porque prueba.provocarMiExcepcion() condujo al bloque catch
+		System.out.println("Continuando ejecución del programa");
+		try {
+		System.out.println("Llamando la funci&oacute;n lanzarExcepcionEnTiempoEjecucion() fuera del bloque try-catch");
+		prueba.lanzarExcepcionEnTiempoEjecucion();
+		}catch(RuntimeException re) {
+			System.err.println("Atrapando excepción para continuar la ejecución del programa.");
+			re.printStackTrace();
+			System.err.println("Mensaje de error: " + re.getMessage());
+		}
+		
+		System.out.println("Continuando ejecución del programa");
 		try {
 		System.out.println("Llamando la funci&oacute;n provocarExcepcionEnTiempoEjecucion() fuera del bloque try-catch");
 		prueba.provocarExcepcionEnTiempoEjecucion();
 		}catch(ArithmeticException ae) {
+			System.err.println("Atrapando excepción para continuar la ejecución del programa.");
 			ae.printStackTrace();
 			System.err.println("Mensaje de error: " + ae.getMessage());
 		}
+		
+		System.out.println("Fin del programa");
 		
 	}
 }
